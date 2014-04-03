3d52cdca31453f3c980b94bd4be8039b64fb0483
Arnav Roy <roy.arnav@gmail.com>
2014-04-02 23:41:43 -0700

    P01 solution.

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,9 +1,16 @@
 // Refer the LICENSE.md file in this repo for information regarding copyright.
- 
+
+// P01: Find the last element of a list.
+// Example:
+// scala> last(List(1, 1, 2, 3, 5, 8))
+// res0: Int = 8
+
 package src.main.scala
 
 object P01 {
-
-  // Dummy function to check Travis CI setup.
-  def getOne(): Int = 1
-} 
+  def last[T](l: List[T]): T = l match {
+      case Nil => throw new NoSuchElementException
+      case x::Nil => x
+      case x::xs => last(xs)
+  } 
+}
--- src/test/scala/P01Test.scala
+++ src/test/scala/P01Test.scala
@@ -3,8 +3,22 @@ package src.test.scala
 import org.scalatest._
 import src.main.scala.P01
 
-class StackSpec extends UnitSpec {
-  "The getOne function" should "always return 1" in {
-    assert(P01.getOne() == 1)
+class P01Spec extends UnitSpec {
+  "last function" should "return NoSuchElementException for empty list" in {
+    intercept[NoSuchElementException] {
+      P01.last(List())
+    }
+  }
+
+  it should "return 1 for List(1)" in {
+    assert(P01.last(List(1)) == 1)
+  }
+
+  it should "return 2 for List(1, 2)" in {
+    assert(P01.last(List(1, 2)) == 2)
+  }
+
+  it should "return b for List('a', 'b')" in {
+    assert(P01.last(List('a', 'b')) == 'b')
   }
 }
